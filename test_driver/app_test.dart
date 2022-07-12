0971559bd00c20a6a191565491dc9709a29b66c2
Gabini - Mac <haile.mak11@gmail.com>
2022-07-12 12:02:44 +0300

    initialize create_note feature with gherkin

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -3,16 +3,25 @@ import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
 
+import 'steps/steps.dart';
+
 Future<void> main() {
   final config = FlutterTestConfiguration()
-    ..features = [RegExp('features/*.*.feature')]
+    ..features = [RegExp('test_driver/features/*.*.feature')]
     ..reporters = [
       ProgressReporter(),
       TestRunSummaryReporter(),
       JsonReporter(path: './report.json')
     ] // you can include the "StdoutReporter()" without the message level parameter for verbose log information
 
-    ..stepDefinitions = []
+    ..stepDefinitions = [
+      GivenIWantToCreateANewNoteStep(),
+      WhenItapButtonStep(),
+      ThenIShouldBeNavigatedToNewNoteStep(),
+      GivenIHaveTypedTextStep(),
+      WhenISaveTheNoteStep(),
+      ThenISaveTheNoteStep(),
+    ]
     ..customStepParameterDefinitions = []
     ..restartAppBetweenScenarios = true
     ..targetAppPath = "test_driver/app.dart";
--- test_driver/features/create_note.feature
+++ test_driver/features/create_note.feature
@@ -5,6 +5,9 @@ Feature: Create note
     so that I can save them.
     Scenario: Create a note 
         Given I want to create a note 
-        When I type "Hello World" 
-        And I save the note
-        Then It should be saved successfully 
+        When I tap the "New note" button        
+        Then I should be navigated to an empty note
+    Scenario: Save a new note         
+        Given I have typed "Hello World"
+        When I save the note
+        Then The text "Hello World" should be saved successfully
